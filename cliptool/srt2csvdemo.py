8bd6f17731eba3fa8693352be7c24f2541eddf6a
scixing <whs11998@163.com>
2024-05-08 15:30:04 +0800

    update

--- cliptool/srt2csvdemo.py
+++ cliptool/srt2csvdemo.py
@@ -12,6 +12,7 @@
 import pysubs2
 import csv
 import os
+import pysrt
 
 def convert_ass_to_csv(ass_file_path, csv_file_path):
     # 加载 ASS 文件
@@ -38,10 +39,27 @@ def convert_ass_to_csv(ass_file_path, csv_file_path):
             # 将字幕的开始时间、结束时间和文本写入 CSV
             writer.writerow(["", line.text, start_time, end_time])
 
+def convert_srt_to_csv(srt_file_path, csv_file_path):
+    # 加载 SRT 文件
+    subs = pysrt.open(srt_file_path, encoding="utf-8")
+    
+    # 创建或覆盖 CSV 文件，并写入字幕信息
+    with open(csv_file_path, mode='w', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        writer.writerow(["人物", "人物台词", "开始时间", "结束时间"])  # 写入表头
+        
+        for line in subs:
+            # 将字幕的开始时间、结束时间转换为时间字符串
+            # 将字幕的开始时间、结束时间和文本写入 CSV
+            start_time = str(line.start.to_time())
+            end_time = str(line.end.to_time())
+
+            writer.writerow(["", line.text, start_time, end_time])
 
 def main():
 
-    ass_folder = "D:/haruhivideo/[CASO][Suzumiya_Haruhi_no_Yuuutsu][BDRIP][Subtitle_SC][ASS]/"
+    # ass_folder = "D:/haruhivideo/[CASO][Suzumiya_Haruhi_no_Yuuutsu][BDRIP][Subtitle_SC][ASS]/"
+    ass_folder = "out"
     file_list = os.listdir(ass_folder)
 
     for f in file_list:
@@ -52,3 +70,16 @@ def main():
                 convert_ass_to_csv(ass_file_path, csv_file_path)
             except Exception as e:
                 print(f"Error processing {f}: {e}")
+
+        elif f.endswith(".srt"):
+            try:
+                ass_file_path = os.path.join(ass_folder, f)
+                csv_file_path = os.path.join("outsrtcsv1", f.replace(".ass", ".csv"))
+                convert_srt_to_csv(ass_file_path, csv_file_path)
+            except Exception as e:
+                print(f"Error processing {f}: {e}")
+            
+
+            
+if __name__ == "__main__":
+    main()
